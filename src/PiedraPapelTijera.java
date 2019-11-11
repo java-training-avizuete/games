d28ec6f553aa43433b99b4a526c54c147b8c3903
Antonio Vizuete <antonio.vizuete.sanchis@everis.com>
2019-11-11 09:15:42 +0100

    Refactorización del monolito

--- src/PiedraPapelTijera.java
+++ src/PiedraPapelTijera.java
@@ -3,20 +3,61 @@ import java.util.Scanner;
 
 public class PiedraPapelTijera {
 
-	public static void main(String[] args) {
+	public static void main(String[] agrs) {
 		System.out.println("Bienvenidos a Piedra, Papel, Tijera!");
-		Scanner scanner = new Scanner(System.in);
+
 		Random random = new Random();
 
 		int contadorUsuario = 0;
 		int contadorMaquina = 0;
 
 		while (contadorMaquina < 3 && contadorUsuario < 3) {
-			System.out.println("Juegue: ");
-			int jugadaComp = random.nextInt(3);
-			String jugada = scanner.nextLine().toLowerCase();
-			int jugadaUsuario;
 
+			int jugadaComp = random.nextInt(3);
+			int jugadaUsuario = recogeJugadaUsuario();
+
+			imprimirVs(jugadaComp, jugadaUsuario);
+
+			if (jugadaComp == jugadaUsuario) {
+				System.out.println("Empate!");
+			} else if ((jugadaComp == 0 && jugadaUsuario == 2) || (jugadaComp == 1 && jugadaUsuario == 0)
+					|| (jugadaComp == 2 && jugadaUsuario == 1)) {
+				contadorMaquina++;
+			} else {
+				contadorUsuario++;
+			}
+
+			System.out.printf("Jugador: %d | Máquina: %d \n", contadorUsuario, contadorMaquina);
+		}
+
+		System.out.println("Fin!");
+	}
+
+	private static void imprimirVs(int jugadaOrdenador, int jugadaUsaurio) {
+		System.out.printf("%s vs. %s\n", jugadaALiteral(jugadaUsaurio), jugadaALiteral(jugadaOrdenador));
+	}
+
+	private static String jugadaALiteral(int jugada) {
+		switch (jugada) {
+		case 0:
+			return "Piedra";
+		case 1:
+			return "Papel";
+		case 2:
+			return "Tijera";
+		default:
+			return null;
+		}
+	}
+
+	private static int recogeJugadaUsuario() {
+		
+		Scanner scanner = new Scanner(System.in);
+		int jugadaUsuario;
+
+		do {
+			System.out.println("Juegue: ");
+			String jugada = scanner.nextLine().toLowerCase();
 			switch (jugada) {
 			case "piedra":
 				jugadaUsuario = 0;
@@ -32,28 +73,11 @@ public class PiedraPapelTijera {
 				System.err.println("Jugada no soportada!! ");
 				break;
 			}
-
-			if (jugadaUsuario == -1) {
-				scanner.close();
-				throw new RuntimeException("Jugada no soportada");
-			}
-
-			System.out.printf("%d vs. %d\n", jugadaUsuario, jugadaComp);
-
-			if (jugadaComp == jugadaUsuario) {
-				System.out.println("Empate!");
-			} else if ((jugadaComp == 0 && jugadaUsuario == 2) || (jugadaComp == 1 && jugadaUsuario == 0)
-					|| (jugadaComp == 2 && jugadaUsuario == 1)) {
-				contadorMaquina++;
-			} else {
-				contadorUsuario++;
-			}
-
-			System.out.printf("Jugador: %d | Máquina: %d \n", contadorUsuario, contadorMaquina);
-		}
-
+		} while (jugadaUsuario == -1);
+		
 		scanner.close();
-		System.out.println("Fin!");
+		
+		return jugadaUsuario;
 	}
 
 }
